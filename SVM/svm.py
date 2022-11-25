e809ec488f99bb1e9f3b628afe354f53c1957e5d
BooSung Jung <68157794+boosungjung@users.noreply.github.com>
2022-11-25 13:47:14 +0800

    kmeans and svm

--- SVM/svm.py
+++ SVM/svm.py
@@ -1,13 +1,25 @@
 import sklearn
 from sklearn import datasets
-from sklearn import svm
+from sklearn import svm, metrics
+from sklearn.neighbors import KNeighborsClassifier
 
 cancer = datasets.load_breast_cancer()
-
-print(cancer.feature_names)
 
 x = cancer.data
 y = cancer.target
 
 x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size=0.2)
 classes = ["malignant", "benign"]
+
+clf = svm.SVC(kernel="linear", C=1)  # support vector classification
+clf.fit(x_train, y_train)
+y_pred = clf.predict(x_test)
+acc = metrics.accuracy_score(y_test,y_pred)
+print(acc)
+
+clfKnn = KNeighborsClassifier(n_neighbors=9)
+clfKnn.fit(x_train, y_train)
+y_pred = clfKnn.predict(x_test)
+acc = metrics.accuracy_score(y_test,y_pred)
+
+print(acc)
